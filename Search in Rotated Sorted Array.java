4ab9e42840c8e729949b7bd996f116fad74fe573
Miraj Khan <92203479+Miraj06@users.noreply.github.com>
2023-07-26 01:04:07 +0530

    Create Search in Rotated Sorted Array.java
    https://leetcode.com/problems/search-in-rotated-sorted-array/description/

---
+++ Search in Rotated Sorted Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int val=0;
+        for(int i=0; i<nums.length;i++){
+            if(nums[i]==target){
+                val=i;
+                break;
+            }
+            else{
+                val=-1;
+            }
+        }
+        return val;
+    }
+}
